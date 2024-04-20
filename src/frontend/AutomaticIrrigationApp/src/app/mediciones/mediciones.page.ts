689b882563af80882afd70d1991023c76d2fc339
Enzo Andreetto <enzo.andreetto@gmail.com>
2024-04-20 10:18:17 -0300

    Implement measurements ui

--- src/frontend/AutomaticIrrigationApp/src/app/mediciones/mediciones.page.ts
+++ src/frontend/AutomaticIrrigationApp/src/app/mediciones/mediciones.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MedicionService } from '../services/medicion.service';
 import { Medicion } from '../models/Medicion';
-import { take, tap } from 'rxjs';
+import { Observable, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-mediciones',
@@ -12,7 +12,7 @@ import { take, tap } from 'rxjs';
 export class MedicionesPage implements OnInit {
 
     dispositivoId?: number;
-    mediciones?: Medicion[];
+    mediciones$?: Observable<Medicion[]>;
 
     constructor(
         private _router: ActivatedRoute,
@@ -21,9 +21,6 @@ export class MedicionesPage implements OnInit {
     ngOnInit() {
         const idParam = this._router.snapshot.paramMap.get('dispositivoId');
         this.dispositivoId = idParam ? parseInt(idParam, 10) : -1;
-        this._medicionService.getMedicionesDeDispositivo(this.dispositivoId).pipe(
-            take(1),
-            tap(mediciones => this.mediciones = mediciones)
-        ).subscribe();
+        this.mediciones$ = this._medicionService.getMedicionesDeDispositivo(this.dispositivoId);
     }
 }
